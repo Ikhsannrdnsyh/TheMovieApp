297d991bd382f78f57b1ba4915de167080170395
Ikhsannrdnsyh <ikhsannurdiansyah78@gmail.com>
2024-12-24 21:17:02 +0700

    Initial Commit

---
+++ TheMovieApp/TheMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieAppApp.swift
+//  TheMovieApp
+//
+//  Created by Mochamad Ikhsan Nurdiansyah on 24/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
